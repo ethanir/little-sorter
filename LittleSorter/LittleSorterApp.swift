9385ce3ab002d1e2211b78fdeefc41559ac309e3
Ethan Irimiciuc <ethanirimiciuc@gmail.com>
2026-05-23 18:49:08 -0500

    Initial Commit

---
+++ LittleSorter/LittleSorterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleSorterApp.swift
+//  LittleSorter
+//
+//  Created by Ethan Irimiciuc on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleSorterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
